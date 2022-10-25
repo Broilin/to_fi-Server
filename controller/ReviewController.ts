9e557e83f053d78fe19fbb1815bb376cd26aa792
N3wCP <61874428+Broilin@users.noreply.github.com>
2022-10-25 21:07:57 +0900

    Add status code

--- controller/ReviewController.ts
+++ controller/ReviewController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 const postReview = async (req: Request, res: Response) => {
   console.log(req.body);
+  res.sendStatus(201);
 };
 
 export { postReview };
